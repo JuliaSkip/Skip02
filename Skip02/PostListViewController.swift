f3f3d02f590c5279ff85b23f15df38ae2e01f9a9
Julia Skip <skipjulija01@gmail.com>
2025-03-15 20:59:10 +0200

    fixed all

---
+++ Skip02/PostListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PostListViewController.swift
+//  Skip02
+//
+//  Created by Скіп Юлія Ярославівна on 15.03.2025.
+//
+
+import Foundation
+import UIKit
+
+class PostListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
